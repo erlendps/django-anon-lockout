f92593ff8723ecb9879d900b39b212e9a1cbbbc9
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-04-01 16:13:34 +0200

    add models used in app #1

--- anon_lockout/models.py
+++ anon_lockout/models.py
@@ -1,3 +1,35 @@
+"""Models for anonymous lockout."""
+
 from django.db import models
 
-# Create your models here.
+
+class Attempt(models.Model):
+    """
+    An attempt has an IP-address, data wheter the attempt was successful or not,
+    and which resource which was tried to be accessed.
+    """
+    success = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class AccessSession(models.Model):
+    """
+    An access session can be abstracted to a user session. It holds information
+    about attempts and resources accessed. 
+    """
+
+    ip = models.CharField(max_length=256)
+    failed_in_row = models.IntegerField()
+
+
+class Lockout(models.Model):
+    """
+    Model representing a lockout, specifying which IP-address
+    is locked out, when it is unlocked and which resource the IP-address is
+    locked out from.
+    """
+
+    session = models.ForeignKey(
+        to=AccessSession, on_delete=models.DO_NOTHING, null=True)
+    ip = models.CharField(max_length=256)
+    unlocks_on = models.DateTimeField()
