68e9fd143c150285a5ed782d3a7b588e2ec791a1
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-04-05 13:29:50 +0200

    add some documentation

--- anon_lockout/conf.py
+++ anon_lockout/conf.py
@@ -3,10 +3,12 @@
 import os
 
 # How many attemps a user should get before being locked out.
+# default 5 attempts
 LOCKOUT_THRESHOLD = os.environ.get("LOCKOUT_THRESHOLD", 5)
 
-# Lockout time in seconds
+# Lockout time in seconds, default 1 day
 LOCKOUT_DURATION = os.environ.get("LOCKOUT_DURATION", 86400)
 
 # The time between two attempts that resets (zeros) the lockout threshold, in seconds
+# default 30 minutes
 LOCKOUT_RESET_TIME = os.environ.get("LOCKOUT_RESET_TIME", 1800)
