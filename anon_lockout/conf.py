52a3dea597103575cca41896a0b794b552eae684
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-04-08 15:14:46 +0200

    use django settings for overriding module settings

--- anon_lockout/conf.py
+++ anon_lockout/conf.py
@@ -1,14 +1,13 @@
 """Settings that can be configured."""
-
-import os
+from django.conf import settings
 
 # How many attemps a user should get before being locked out.
 # default 5 attempts
-LOCKOUT_THRESHOLD = os.environ.get("LOCKOUT_THRESHOLD", 5)
+LOCKOUT_THRESHOLD = getattr(settings, "LOCKOUT_THRESHOLD", 5)
 
 # Lockout time in seconds, default 1 day
-LOCKOUT_DURATION = os.environ.get("LOCKOUT_DURATION", 86400)
+LOCKOUT_DURATION = getattr(settings, "LOCKOUT_DURATION", 86400)
 
 # The time between two attempts that resets (zeros) the lockout threshold, in seconds
 # default 30 minutes
-LOCKOUT_RESET_TIME = os.environ.get("LOCKOUT_RESET_TIME", 1800)
+LOCKOUT_RESET_TIME = getattr(settings, "LOCKOUT_RESET_TIME", 1800)
