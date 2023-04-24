140f92379f65c86dd0a6a00b611ac8f5d3098d36
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-04-24 11:47:28 +0200

    refactor: change lockout threshold

--- anon_lockout/conf.py
+++ anon_lockout/conf.py
@@ -2,8 +2,8 @@
 from django.conf import settings
 
 # How many attemps a user should get before being locked out.
-# default 5 attempts
-LOCKOUT_THRESHOLD = getattr(settings, "LOCKOUT_THRESHOLD", 5)
+# default 100 attempts
+LOCKOUT_THRESHOLD = getattr(settings, "LOCKOUT_THRESHOLD", 100)
 
 # Lockout time in seconds, default 1 day
 LOCKOUT_DURATION = getattr(settings, "LOCKOUT_DURATION", 86400)
