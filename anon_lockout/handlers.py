f443f50dcf0fa4ce147042397b0fd533f668cf11
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-04-24 12:17:49 +0200

    docs: add new docs to handlers

--- anon_lockout/handlers.py
+++ anon_lockout/handlers.py
@@ -12,6 +12,8 @@ def handle_attempt(ip: str, failed: bool, resource: str) -> bool:
     It takes in a str (ip) and boolean. It uses the request to fetch
     the ip of the attempt, while the boolean indicates wheter the attempt
     was successful or not.
+
+    Returns True if the session is locked out, False otherwise.
     """
 
     # create the attempt
@@ -22,18 +24,24 @@ def handle_attempt(ip: str, failed: bool, resource: str) -> bool:
     attempt.session = session
     attempt.save()
 
+    # check if there is an lockout connected to this session
     lockout: Lockout = Lockout.objects.get_or_none(session=session)
     if lockout != None:
+        # if the lockout has expired
         if lockout.unlocks_on <= attempt.date:
             lockout.active = False
             lockout.save()
             session.failed_in_row = 0
             session.save()
+        # if it has not expired
         else:
+            # also add extra stats
             if failed:
                 session.failed_in_row += 1
                 session.save()
+            # it is locked, return True
             return True
+    # delegate to handlers based on failed
     if failed:
         return handle_failed_attempt(attempt=attempt)
     else:
@@ -43,13 +51,18 @@ def handle_attempt(ip: str, failed: bool, resource: str) -> bool:
 def handle_failed_attempt(attempt: Attempt) -> bool:
     """
     Handles the attempt if it was failed.
+
+    Increments failed_in_row and updates last access date. Also
+    checks if the session should be locked out.
     """
 
     session = attempt.session
+    # increment and update
     session.failed_in_row += 1
     session.last_access = attempt.date
     session.save()
 
+    # check if it should lock
     if session.failed_in_row >= LOCKOUT_THRESHOLD and not session.has_active_lockout():
         unlocks = session.last_access + timedelta(seconds=LOCKOUT_DURATION)
         Lockout.objects.create(session=session, unlocks_on=unlocks)
@@ -60,6 +73,8 @@ def handle_failed_attempt(attempt: Attempt) -> bool:
 def handle_successful_attempt(attempt: Attempt) -> bool:
     """Handles a successful attempt."""
 
+    # increment
     attempt.session.successes += 1
     attempt.session.save()
+    # obviously not locked
     return False
