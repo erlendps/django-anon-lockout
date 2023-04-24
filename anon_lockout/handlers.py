761129567e7a55defe0177293ef4588e7d372ea6
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-04-24 11:43:59 +0200

    refactor: remove resetting of counter when successful

--- anon_lockout/handlers.py
+++ anon_lockout/handlers.py
@@ -64,6 +64,4 @@ def handle_failed_attempt(attempt: Attempt) -> bool:
 
 def handle_successful_attempt(attempt: Attempt) -> bool:
     """Handles a successful attempt."""
-    attempt.session.failed_in_row = 0
-    attempt.session.save()
     return True
